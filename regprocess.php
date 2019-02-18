d3eb9c996dd146c02c86865e258d964d3dbe7ce6
vedantdharwal25 <vedantdharwal25@gmail.com>
2019-02-18 09:39:46 +0530

    Update regprocess.php

--- regprocess.php
+++ regprocess.php
@@ -4,7 +4,7 @@ if($db->connect_error)
 {
     die("Connection failed : ".$db->connect_error);
 }
-$id=$_POST["id"];
+//$id=$_POST["id"];
 $fname=$_POST["fname"];
 $lname=$_POST["lname"];
 $mobilenum=$_POST["mobilenum"];
@@ -14,6 +14,6 @@ $collegename=$_POST["collegename"];
 $email=$_POST["email"];
 $password=$_POST["password"];
 $acctype="u";
-$ins=mysqli_query($db,"insert into users values('$id','$fname','$lname','$mobilenum','$usn','$sem','$collegename','$email','$password')");
+$ins=mysqli_query($db,"insert into users values($fname','$lname','$mobilenum','$usn','$sem','$collegename','$email','$password')");
 header("location:login.php");
 ?>
