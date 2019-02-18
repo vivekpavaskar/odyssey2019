75d74ffab6cc6adfc95d91ffbccac51054a66f87
vedantdharwal25 <vedantdharwal25@gmail.com>
2019-02-18 11:19:39 +0530

    Regis Part 1

--- regprocess.php
+++ regprocess.php
@@ -13,6 +13,7 @@ $sem=$_POST["sem"];
 $collegename=$_POST["collegename"];
 $email=$_POST["email"];
 $password=$_POST["password"];
+
 $acctype="u";
 $ins=mysqli_query($db,"insert into users values($fname','$lname','$mobilenum','$usn','$sem','$collegename','$email','$password')");
 header("location:login.php");
